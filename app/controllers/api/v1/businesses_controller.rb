5cf093cc951bf7542b8ebfc4b2df711271251336
ksummerill <kaitlynsummerill@gmail.com>
2020-10-05 13:57:10 -0700

    add create controller action and route

--- app/controllers/api/v1/businesses_controller.rb
+++ app/controllers/api/v1/businesses_controller.rb
@@ -1,6 +1,22 @@
 class Api::V1::BusinessesController < ApplicationController
 
   def index
-    @businesses = Business.all
+    businesses = Business.all
+    render json: businesses
+  end
+
+  def create
+    business = Business.new(business_params)
+    if business.save
+      render json: business, status: :accepted
+    else
+      render json: {errors: business.errors.full_messages}, status: :unprocessible_entity
+    end
+  end
+
+  private
+
+  def business_params
+    params.require(:business).permit(:name, :street_address, :city, :state, :zipcode, :description, :website, :category_id)
   end
 end
